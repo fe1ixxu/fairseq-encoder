7106fd506059b4a91691574f5ea0f2fcb8e00c50
Haoran Xu <haoranxu@c02.clsp.jhu.edu>
2021-11-02 04:59:06 -0400

    for bibert

--- train.sh
+++ train.sh
@@ -1,55 +1,51 @@
-MAX_UPDATE=400000 # 200000
-WARMUP=10000
-DECAY_STEP=200000
-INTERVAL=2500 # 625 or 10000
+MAX_UPDATE=600000 # 200000
+WARMUP=30000
+# DECAY_STEP=200000
+INTERVAL=500 # 625 or 10000
 
 # effective batch size (8 GPUs) = 8*32*8 = 2048
 SEQ_LEN=512
-BS=8
+# BS=8
 FREQ=32 # 128 or 8
-LR=2e-4
-MIN_LR=5e-5
+LR=4e-4
+# MIN_LR=5e-5
 
 # max_positions=512
 
 ARCH=roberta_large
-DATA_PATH=/srv/local2/shijie/data/EnFa-128K-bin
-CODENAME=EnFa-large-128K
-RESTORE_CKPT=/srv/local2/shijie/pretrained/xlmr.large/model.pt
+DATA_PATH=/export/c01/haoranxu/oscar/en-zh-databin
+STORE_PATH=/export/c01/haoranxu/LMs/temp
+# RESTORE_CKPT=/srv/local2/shijie/pretrained/xlmr.large/model.pt
+DATABINS=$(python third_party/get_databin_list.py --start 1 --end 4 --data_path $DATA_PATH)
+echo $DATABINS
+# mkdir -p /srv/local2/shijie/checkpoints/$CODENAME
 
-mkdir -p /srv/local2/shijie/checkpoints/$CODENAME
-
-MKL_THREADING_LAYER=GNU fairseq-train \
-$DATA_PATH/shard0:$DATA_PATH/shard1:$DATA_PATH/shard2:$DATA_PATH/shard3:$DATA_PATH/shard4:$DATA_PATH/shard5:$DATA_PATH/shard6:$DATA_PATH/shard7:$DATA_PATH/shard8:$DATA_PATH/shard9 \
---save-dir /srv/local2/shijie/checkpoints/$CODENAME \
---tensorboard-logdir /srv/local2/shijie/tensorboard/$CODENAME \
---restore-file $RESTORE_CKPT \
+CUDA_VISIBLE_DEVICES=0,1 fairseq-train $DATABINS \
+--save-dir ${STORE_PATH} \
+--tensorboard-logdir ${STORE_PATH} \
 --train-subset train \
 --fp16 \
 --memory-efficient-fp16 \
 --num-workers 4 \
---task multilingual_masked_lm \
+--task masked_lm \
 --criterion masked_lm \
 --arch $ARCH \
 --sample-break-mode complete \
 --tokens-per-sample $SEQ_LEN \
 --optimizer adam \
---adam-betas "(0.9, 0.98)" \
+--adam-betas "(0.9, 0.999)" \
 --adam-eps 1e-6 \
 --clip-norm 1.0 \
 --lr-scheduler polynomial_decay \
 --lr $LR \
---end-learning-rate $MIN_LR \
 --warmup-updates $WARMUP \
---total-num-update $DECAY_STEP \
 --dropout 0.1 \
 --attention-dropout 0.1 \
 --weight-decay 0.01 \
---max-tokens 8192 \
---max-sentences $BS \
+--max-tokens 4096 \
 --update-freq $FREQ \
 --max-update $MAX_UPDATE \
---multilang-sampling-alpha 0.3 \
+--total-num-update $MAX_UPDATE \
 --required-batch-size-multiple 8 \
 --empty-cache-freq 100 \
 --skip-invalid-size-inputs-valid-test \
@@ -60,5 +56,11 @@ $DATA_PATH/shard0:$DATA_PATH/shard1:$DATA_PATH/shard2:$DATA_PATH/shard3:$DATA_PA
 --validate-interval $INTERVAL \
 --save-interval-updates $INTERVAL \
 --no-epoch-checkpoints \
---distributed-world-size 8 \
-| tee /srv/local2/shijie/checkpoints/$CODENAME/train.log
+--distributed-world-size 2 \
+| tee ${STORE_PATH}/train.log
+
+# --end-learning-rate $MIN_LR \
+# --total-num-update $DECAY_STEP \
+#--max-sentences $BS \
+# --restore-file $RESTORE_CKPT \
+# --multilang-sampling-alpha 0.3 \
